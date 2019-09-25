4681300b474345eda54e8c7f89dba1833daa27d9
Kamaal Farah <kamaal.f1@gmail.com>
2019-09-25 19:24:32 +0200

    Displaying opponents name

--- src/Screens/GameScreen/index.js
+++ src/Screens/GameScreen/index.js
@@ -1,12 +1,33 @@
+/* eslint-disable react/no-array-index-key */
 import React, { useEffect, useState } from 'react';
 import { Text, View, Image, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 
 import styles from './styles';
 
-function ImageView({ children, turn, assetId }) {
+function ImageView({
+  io,
+  lobbyId,
+  userId,
+  children,
+  turn,
+  setTurn,
+  setChoice,
+  chosenAsset,
+  assetId,
+}) {
   return turn ? (
-    <TouchableOpacity onPressIn={() => console.log('assetId', assetId)}>
+    <TouchableOpacity
+      onPressIn={() => {
+        setTurn(false);
+        setChoice(assetId);
+        return io.emit('user-in-game-choice-from-client', {
+          userId,
+          lobbyId,
+          choice: chosenAsset,
+        });
+      }}
+    >
       {children}
     </TouchableOpacity>
   ) : (
@@ -14,18 +35,31 @@ function ImageView({ children, turn, assetId }) {
   );
 }
 
-function GameScreen({ users, navigation }) {
-  const [turn] = useState(true);
+function GameScreen({ users, lobbies, navigation }) {
+  const [turn, setTurn] = useState(true);
+  const [choice, setChoice] = useState(null);
 
-  const { io, id } = navigation.state.params;
+  const { io, id: lobbyId } = navigation.state.params;
 
   useEffect(() => {
     io.emit('user-in-lobby-from-client', {
-      lobbyId: id,
+      lobbyId,
       userId: users.activeUser.id,
     });
+    io.on(`user-in-game-${lobbyId}-choice-from-server`, data =>
+      console.log('data', data),
+    );
+    io.on(`user-in-game-${lobbyId}-winner-from-server`, data =>
+      console.log('data', data),
+    );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const currentLobby = lobbies.lobbyList.find(lobby => lobby.id === lobbyId);
+  const opponent =
+    currentLobby.users.length < 2
+      ? null
+      : currentLobby.users.find(user => user.id !== users.activeUser.id);
 
   const assets = [
     require('../../assets/rock.png'),
@@ -33,24 +67,57 @@ function GameScreen({ users, navigation }) {
     require('../../assets/scissors.png'),
   ];
 
+  const assetChoice = index => {
+    if (index === 0) return 'rock';
+    if (index === 1) return 'paper';
+    return 'scissors';
+  };
+
   return (
     <View style={styles.screenContainer}>
       <View style={styles.gameContainer}>
         <Text style={styles.gameTitle}>🔥ROCK PAPER SCISSORS🔥</Text>
+        <Text style={styles.scoreText}>{currentLobby.score}</Text>
         <Text style={styles.activeUserText}>{users.activeUser.name}</Text>
         <View style={styles.assetImagesContainer}>
           {assets.map((asset, i) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <ImageView key={i} turn={turn} assetId={i}>
+            <ImageView
+              key={i}
+              io={io}
+              turn={turn}
+              setTurn={setTurn}
+              userId={users.activeUser.id}
+              lobbyId={lobbyId}
+              setChoice={setChoice}
+              assetId={i}
+              chosenAsset={assetChoice(i)}
+            >
               <Image source={asset} style={styles.assetImage} />
             </ImageView>
           ))}
         </View>
+        {choice === null ? (
+          <></>
+        ) : (
+          <Image source={assets[choice]} style={styles.assetImage} />
+        )}
+        {currentLobby.users.length < 2 ? (
+          <></>
+        ) : (
+          <>
+            <View style={styles.assetImagesContainer}>
+              {assets.map((asset, i) => (
+                <Image key={i} source={asset} style={styles.assetImage} />
+              ))}
+            </View>
+            <Text style={styles.activeUserText}>{opponent.name}</Text>
+          </>
+        )}
       </View>
     </View>
   );
 }
 
-const mapStateToProps = ({ users }) => ({ users });
+const mapStateToProps = ({ users, lobbies }) => ({ users, lobbies });
 
 export default connect(mapStateToProps)(GameScreen);
--- src/theme.js
+++ src/theme.js
@@ -4,4 +4,5 @@ export const primaryColor = '#000000';
 export const secondaryColor = '#0bd9c4';
 export const smallFontSize = screenHeight / 60;
 export const mediumFontSize = screenHeight / 50;
+export const largeFontSize = screenHeight / 30;
 export const borderRadius = screenWidth / 50;
