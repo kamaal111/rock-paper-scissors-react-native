d2280484fe800ebb30c10e6a18931042ccf50284
Kamaal Farah <kamaal.f1@gmail.com>
2020-04-04 18:34:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 /* eslint-disable import/prefer-default-export */
 
-// export const baseUrl = 'http://127.0.0.1:4000';
-export const baseUrl = 'https://frozen-anchorage-04643.herokuapp.com';
+export const baseUrl = 'http://127.0.0.1:4000';
