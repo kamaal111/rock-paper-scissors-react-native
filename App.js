3f88414de7c461ff69a0799e4bc3e9247e87d27f
prodeveloper <dany.ourapps99@gmail.com>
2020-04-05 17:48:50 +1000

    simpler code

--- App.js
+++ App.js
@@ -7,23 +7,18 @@
  */
 
 import React from 'react';
-import { YellowBox } from 'react-native';
-import { Provider } from 'react-redux';
-
-import AppNavigator from './src/AppNavigator';
-
-import store from './src/store';
+import { YellowBox, View, Text } from 'react-native';
 
 YellowBox.ignoreWarnings([
   'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`,' +
-    ' `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`.' +
-    ' Did you mean to put these under `headers`?',
+  ' `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`.' +
+  ' Did you mean to put these under `headers`?',
 ]);
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <AppNavigator />
-    </Provider>
+    <View>
+      <Text>SIMPLER CODE</Text>
+    </View>
   );
 }
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-
-import users from './users';
-import lobbies from './lobbies';
-
-export default combineReducers({ users, lobbies });
